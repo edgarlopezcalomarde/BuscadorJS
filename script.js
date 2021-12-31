602f57cb6945355c52319a5ecb96460dcc7d0cfd
Edgar Lopez <theskril70@gmail.com>
2021-12-31 03:10:21 +0100

    v1

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("keyup", e=>{
+
+  if (e.target.matches("#buscador")){
+
+      if (e.key ==="Escape")e.target.value = ""
+
+      document.querySelectorAll(".articulo").forEach(fruta =>{
+
+          fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?fruta.classList.remove("filtro")
+            :fruta.classList.add("filtro")
+      })
+
+  }
+
+
+})
